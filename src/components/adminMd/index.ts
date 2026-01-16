faae44abe895d2a4a15d902cfa8d9ec03fdd5c23
Sweat-Signal <admin@sweatsignal.com>
2026-01-16 05:26:47 -0500

    refactor: Remove obsolete admin components and related exports
    - Deleted AdminCard, AdminStatCard, AdminTable, and associated exports from the adminMd directory to streamline the codebase and eliminate unused components.
    - This cleanup enhances maintainability and reduces clutter in the project.

--- src/components/adminMd/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './AdminCard'
-export * from './AdminStatCard'
-export * from './AdminTable'
-export * from './MdButton'
-export * from './MdBadge'
-export * from './MdModal'
-export * from './MdFormInput'
-
