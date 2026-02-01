4fd8170f3277fbcac8e08ae3b68c9337be496bd6
mojjammil <mojjammil@gmail.com>
2026-02-01 07:44:23 +0200

    created new baseline migration

--- supabase/migrations/20260122000003_add_preferred_name_to_athletes.sql
+++
@@ -1,7 +0,0 @@
--- Add preferred_name column to athletes table
--- This field stores the name the athlete prefers to go by (nickname, shortened name, etc.)
-
-ALTER TABLE athletes
-ADD COLUMN IF NOT EXISTS preferred_name TEXT;
-
-COMMENT ON COLUMN athletes.preferred_name IS 'The name the athlete prefers to be called (nickname, goes by name)';
